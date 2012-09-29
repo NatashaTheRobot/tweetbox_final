5e39bf0645551f0c99687ade0450a9883e3976fc
NatashaMurashev <nmurashev@gmail.com>
2012-09-29 16:52:24 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
-#                  root        /                                 users#new
-#                 users GET    /users(.:format)                  users#index
-#                       POST   /users(.:format)                  users#create
-#              new_user GET    /users/new(.:format)              users#new
-#             edit_user GET    /users/:id/edit(.:format)         users#edit
-#                  user PUT    /users/:id(.:format)              users#update
-#                       DELETE /users/:id(.:format)              users#destroy
-#              hashtags GET    /hashtags(.:format)               hashtags#index
-#                       POST   /hashtags(.:format)               hashtags#create
-#           new_hashtag GET    /hashtags/new(.:format)           hashtags#new
-#          edit_hashtag GET    /hashtags/:id/edit(.:format)      hashtags#edit
-#               hashtag PUT    /hashtags/:id(.:format)           hashtags#update
-#                       DELETE /hashtags/:id(.:format)           hashtags#destroy
-# auth_twitter_callback GET    /auth/twitter/callback(.:format)  sessions#create
-#               signout GET    /signout(.:format)                sessions#destroy
-#               hashtag GET    /:twitter_name/:hashtag(.:format) hashtags#show
-#                  user GET    /:twitter_name(.:format)          users#show
-
-
 Thetweetbox::Application.routes.draw do
   root to: "users#new"
   get "/auth/twitter/callback" => "sessions#create"
