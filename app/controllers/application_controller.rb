44e56925a4f7293a558e5e1d3423a34b4780887d
NatashaMurashev <nmurashev@gmail.com>
2012-10-01 12:00:32 -0700

    seperated tweets by hashtag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,26 @@ class ApplicationController < ActionController::Base
     end
     Hash[hashtags.sort]
   end
+
+  def hashtags_with_tweets(tweets)
+    hashtags = {"untagged" => []}
+
+    tweets.each do |tweet|
+      if tweet.hashtags == []
+        hashtags["untagged"] << tweet
+      else
+        tweet.hashtags.each do |hashtag|
+          if hashtags[hashtag.text]
+            hashtags[hashtag.text] << tweet
+          else
+            hashtags[hashtag.text] = [tweet]
+          end
+        end
+      end
+    end
+    untagged = hashtags.delete('untagged')
+    hashtags_sorted = Hash[hashtags.sort]
+    hashtags_sorted['untagged'] = untagged
+    hashtags_sorted
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(:first, :conditions => ["lower(twitter_name) =?", params[:twitter_name].downcase])
-    @tweets = @user.tweets.order("twitter_id DESC").scoped.page(params[:page]).per(50)
-    @hashtags = hashtags_with_nums(@user.tweets)
+    @hashtags_with_nums = hashtags_with_nums(@user.tweets)
+    @hashtags_with_tweets = hashtags_with_tweets(@user.tweets)
   end
 
 end
